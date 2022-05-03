4e886f05876c7cc13b3d49955ed9472db2d8ebf6
liliyah <sara.m.ragab@hotmail.com>
2022-05-03 21:48:32 +0200

    zalgouta app

--- app/src/main/java/funapps/fun/music/zarghtly/MainFragment.kt
+++ app/src/main/java/funapps/fun/music/zarghtly/MainFragment.kt
@@ -1,8 +1,13 @@
 package funapps.`fun`.music.zarghtly
 
 import MainViewModel
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.media.AudioManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,6 +15,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,33 +26,53 @@ import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import funapps.`fun`.music.zarghtly.databinding.FragmentMainBinding
 import funapps.`fun`.music.zarghtly.dataclasses.Music
 import funapps.music.zarghtly.adapters.MusicAdapter
+import org.apache.commons.io.FileUtils
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
 import kotlin.math.log
 
-const val AD_UNIT_ID = "ca-app-pub-3940256099942544/1033173712"
+const val AD_UNIT_ID = "ca-app-pub-2020667111518171/6585379019"
 
 class MainFragment : Fragment() {
     private var mInterstitialAd: InterstitialAd? = null
     private var mAdIsLoading: Boolean = false
     private lateinit var musicViewmodel: MainViewModel
     private lateinit var binding: FragmentMainBinding
-    private lateinit var   madapter:MusicAdapter
-
+    private lateinit var madapter: MusicAdapter
+    private lateinit  var inputStream:InputStream
+   private  var downloadPath:String =""
     var musiclist: ArrayList<Music> = ArrayList()
+
+    companion object {
+
+        var firstAudio: File? = null
+        var secondAudio: File? = null
+        var thirdAudio: File? = null
+        var fourthAudio: File? = null
+        var fifthAudio: File? = null
+        var sixthAudio: File? = null
+        var seventhAudio: File? = null
+        var eighthAudio: File? = null
+        var ninethAudio: File? = null
+        var tenthAudio: File? = null
+        var eleventhAudio: File? = null
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        requireActivity().volumeControlStream=(AudioManager.STREAM_MUSIC)
+        saveaudiosToPhone()
+        requireActivity().volumeControlStream = (AudioManager.STREAM_MUSIC)
         MobileAds.initialize(requireContext()) {}
 
         MobileAds.setRequestConfiguration(
             RequestConfiguration.Builder()
-                .setTestDeviceIds(listOf("ABCDEF012345"))
                 .build()
         )
         loadAd()
         super.onCreate(savedInstanceState)
-
-
-
     }
+
     private fun loadAd() {
         var adRequest = AdRequest.Builder().build()
 
@@ -56,8 +83,8 @@ class MainFragment : Fragment() {
                     Log.d("tag", adError?.message)
                     mInterstitialAd = null
                     mAdIsLoading = false
-                    val error = "domain: ${adError.domain}, code: ${adError.code}, " +
-                            "message: ${adError.message}"
+//                    val error = "domain: ${adError.domain}, code: ${adError.code}, " +
+//                            "message: ${adError.message}"
 
                 }
 
@@ -93,47 +120,47 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
-fun showad(){
-    if (mInterstitialAd == null) {
-        loadAd()
-    } else if (mInterstitialAd != null) {
+    fun showad() {
+        if (mInterstitialAd == null) {
+            loadAd()
+        } else if (mInterstitialAd != null) {
 
-        mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
-            override fun onAdDismissedFullScreenContent() {
-                Log.d("ad", "the ad is dissmissed ")
-                super.onAdDismissedFullScreenContent()
-                requireActivity().finish()
-                mInterstitialAd = null
-                loadAd()
+            mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+                override fun onAdDismissedFullScreenContent() {
+                    Log.d("ad", "the ad is dissmissed ")
+                    super.onAdDismissedFullScreenContent()
+                    requireActivity().finish()
+                    mInterstitialAd = null
+                    loadAd()
+                }
+
+                override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
+                    Log.d("ad", "the ad has failed ")
+
+                    mInterstitialAd = null
+
+                }
+
+                override fun onAdShowedFullScreenContent() {
+                    Log.d("ad", "ad showed full screen ")
+
+                }
             }
 
-            override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
-                Log.d("ad", "the ad has failed ")
-
-                mInterstitialAd = null
-
-            }
-
-            override fun onAdShowedFullScreenContent() {
-                Log.d("ad", "ad showed full screen ")
-
-            }
+            mInterstitialAd?.show(requireActivity())
         }
 
-        mInterstitialAd?.show(requireActivity())
     }
 
-}
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,object :OnBackPressedCallback(true){
-            override fun handleOnBackPressed() {
-                showad()
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    showad()
+                }
+
+
             }
-
-
-        }
-
 
 
         )
@@ -145,5 +172,85 @@ fun showad(){
         super.onDetach()
     }
 
+//    private fun allowPermissionForFile() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//
+////
+////            if (ContextCompat.checkSelfPermission(requireContext(),
+////                    Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+////            ) {
+////                ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+////
+////            }else{
+////                saveaudiosToPhone()
+////
+////            }
+//            downloadPath =
+//                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath + "/"
+//
+//        }else{
+//
+//        }
+//
+//    }
 
+
+    fun saveaudiosToPhone() {
+        try {
+//             downloadPath =
+//                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath + "/"
+
+            firstAudio = File(requireActivity()?.getExternalFilesDir(Environment.DIRECTORY_MUSIC) , "zarghoutaegypt.mp3")
+            inputStream = requireContext().resources.openRawResource(R.raw.zarghoutaegypt)
+            FileUtils.copyInputStreamToFile(inputStream, firstAudio)
+
+
+            secondAudio = File(requireActivity()?.getExternalFilesDir(Environment.DIRECTORY_MUSIC),"zarhoutatunis.mp3")
+            inputStream = requireContext().resources.openRawResource(R.raw.zarhoutatunis)
+            FileUtils.copyInputStreamToFile(inputStream, secondAudio)
+
+            thirdAudio = File(requireActivity()?.getExternalFilesDir(Environment.DIRECTORY_MUSIC), "zarghoutamaroc.mp3")
+            inputStream = requireContext().resources.openRawResource(R.raw.zarghoutamaroc)
+            FileUtils.copyInputStreamToFile(inputStream, thirdAudio)
+
+            fourthAudio = File(requireActivity()?.getExternalFilesDir(Environment.DIRECTORY_MUSIC), "zarghoutapalestine.mp3")
+            inputStream = requireContext().resources.openRawResource(R.raw.zarghoutapalestine)
+            FileUtils.copyInputStreamToFile(inputStream, fourthAudio)
+
+            fifthAudio = File(requireActivity()?.getExternalFilesDir(Environment.DIRECTORY_MUSIC), "zargoutasoudan.mp3")
+            inputStream = requireContext().resources.openRawResource(R.raw.zargoutasoudan)
+            FileUtils.copyInputStreamToFile(inputStream, fifthAudio)
+
+            sixthAudio = File(requireActivity()?.getExternalFilesDir(Environment.DIRECTORY_MUSIC), "zarghoutalebanon.mp3")
+            inputStream = requireContext().resources.openRawResource(R.raw.zarghoutalebanon)
+            FileUtils.copyInputStreamToFile(inputStream, sixthAudio)
+
+            seventhAudio = File(requireActivity()?.getExternalFilesDir(Environment.DIRECTORY_MUSIC),"zarghoutaspecial.mp3")
+            inputStream = requireContext().resources.openRawResource(R.raw.zarghoutaspecial)
+            FileUtils.copyInputStreamToFile(inputStream, seventhAudio)
+
+            eighthAudio = File(requireActivity()?.getExternalFilesDir(Environment.DIRECTORY_MUSIC), "zarghoutakhalegeya.mp3")
+            inputStream = requireContext().resources.openRawResource(R.raw.zarghoutakhalegeya)
+            FileUtils.copyInputStreamToFile(inputStream, eighthAudio)
+
+            ninethAudio = File(requireActivity()?.getExternalFilesDir(Environment.DIRECTORY_MUSIC), "zahroutetelfarah.mp3")
+            inputStream = requireContext().resources.openRawResource(R.raw.zahroutetelfarah)
+            FileUtils.copyInputStreamToFile(inputStream, ninethAudio)
+
+            tenthAudio = File(requireActivity()?.getExternalFilesDir(Environment.DIRECTORY_MUSIC), "zarghoutagroup.mp3")
+            inputStream = requireContext().resources.openRawResource(R.raw.zarghoutagroup)
+            FileUtils.copyInputStreamToFile(inputStream, tenthAudio)
+
+            eleventhAudio = File(requireActivity()?.getExternalFilesDir(Environment.DIRECTORY_MUSIC),"zarghoutaspecial.mp3")
+            inputStream = requireContext().resources.openRawResource(R.raw.zarghoutaspecial)
+            FileUtils.copyInputStreamToFile(inputStream, eleventhAudio)
+            Log.d("saveaudioteveth", "saveaudiosToPhone:${eleventhAudio.toString()} ")
+
+
+        } catch (e: IOException) {
+            Log.d("saveaudiotostorage", "saveaudiosToPhone:${e.toString()} ")
+        }
+    }
 }
+
+
--- app/src/main/java/funapps/fun/music/zarghtly/adapters/MusicAdapter.kt
+++ app/src/main/java/funapps/fun/music/zarghtly/adapters/MusicAdapter.kt
@@ -1,28 +1,25 @@
 package funapps.music.zarghtly.adapters
 
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ResolveInfo
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.net.Uri
-import android.os.Environment
-import android.os.Environment.DIRECTORY_DOWNLOADS
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.SeekBar
+import android.widget.Toast
 import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.RecyclerView
+import funapps.`fun`.music.zarghtly.MainFragment
 import funapps.`fun`.music.zarghtly.R
 import funapps.`fun`.music.zarghtly.databinding.RecyclerItemBinding
 import funapps.`fun`.music.zarghtly.dataclasses.Music
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.apache.commons.io.FileUtils.copyInputStreamToFile
-import java.io.File
 
 
 class MusicAdapter(val musiclist: ArrayList<Music>, private val context: Context) :
@@ -30,18 +27,8 @@ class MusicAdapter(val musiclist: ArrayList<Music>, private val context: Context
     companion object {
 
         private var mediaPlayer: MediaPlayer? = null
-        private var firstAudio: File? = null
-        private var secondAudio: File? = null
-        private var thirdAudio: File? = null
-        private var fourthAudio: File? = null
-        private var fifthAudio: File? = null
-        private var sixthAudio: File? = null
-        private var seventhAudio: File? = null
-        private var eighthAudio: File? = null
-        private var ninethAudio: File? = null
-        private var tenthAudio: File? = null
-        private var eleventhAudio: File? = null
-        private lateinit var path: Uri
+
+       private lateinit var path: Uri
         var handler: Handler = Handler(Looper.getMainLooper())
         var runnable: Runnable = Runnable { }
     }
@@ -50,7 +37,7 @@ class MusicAdapter(val musiclist: ArrayList<Music>, private val context: Context
 
         mediaPlayer = MediaPlayer()
         mediaPlayer!!.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        saveaudiosToPhone()
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicViewHolder {
@@ -91,77 +78,142 @@ class MusicAdapter(val musiclist: ArrayList<Music>, private val context: Context
                     mediaPlayer!!.isLooping = true
 
 
-                    runnable=object : Runnable {
+                    runnable = object : Runnable {
                         override fun run() {
-                            val  current_pos = mediaPlayer!!.getCurrentPosition();
+                            val current_pos = mediaPlayer!!.getCurrentPosition();
                             binding.seekbar.setProgress(current_pos.toInt())
 
                             handler.postDelayed(this, 1000)
                         }
                     }
-                    handler.postDelayed(runnable,0)
+                    handler.postDelayed(runnable, 0)
 
                     binding.btnPlay.setImageResource(R.drawable.pause)
                 }
 
             }
             binding.btnShare.setOnClickListener {
+
+
                 var position = adapterPosition
                 when (position) {
                     0 -> path = FileProvider.getUriForFile(mcontext,
                         "funapps.fun.music.zarghtly.fileprovider",
-                        firstAudio!!)
+                        MainFragment.firstAudio!!)
+
                     1 -> path = FileProvider.getUriForFile(mcontext,
                         "funapps.fun.music.zarghtly.fileprovider",
-                        secondAudio!!)
+                        MainFragment.secondAudio!!)
                     2 -> path = FileProvider.getUriForFile(mcontext,
                         "funapps.fun.music.zarghtly.fileprovider",
-                        thirdAudio!!)
+                        MainFragment.thirdAudio!!)
                     3 -> path = FileProvider.getUriForFile(mcontext,
                         "funapps.fun.music.zarghtly.fileprovider",
-                        fourthAudio!!)
+                        MainFragment.fourthAudio!!)
 
                     4 -> path = FileProvider.getUriForFile(mcontext,
                         "funapps.fun.music.zarghtly.fileprovider",
-                        fifthAudio!!)
+                        MainFragment.fifthAudio!!)
                     5 -> path = FileProvider.getUriForFile(mcontext,
                         "funapps.fun.music.zarghtly.fileprovider",
-                        sixthAudio!!)
+                        MainFragment.sixthAudio!!)
                     6 -> path = FileProvider.getUriForFile(mcontext,
                         "funapps.fun.music.zarghtly.fileprovider",
-                        seventhAudio!!)
+                        MainFragment.seventhAudio!!)
                     7 -> path = FileProvider.getUriForFile(mcontext,
                         "funapps.fun.music.zarghtly.fileprovider",
-                        eighthAudio!!)
+                        MainFragment.eighthAudio!!)
                     8 -> path = FileProvider.getUriForFile(mcontext,
                         "funapps.fun.music.zarghtly.fileprovider",
-                        ninethAudio!!)
+                        MainFragment.ninethAudio!!)
                     9 -> path = FileProvider.getUriForFile(mcontext,
                         "funapps.fun.music.zarghtly.fileprovider",
-                        tenthAudio!!)
+                        MainFragment.tenthAudio!!)
 
 
                     10 -> path = FileProvider.getUriForFile(mcontext,
                         "funapps.fun.music.zarghtly.fileprovider",
-                        eleventhAudio!!)
+                        MainFragment.eleventhAudio!!)
 
                     else -> {
 
                         path = FileProvider.getUriForFile(mcontext,
                             "funapps.fun.music.zarghtly.fileprovider",
-                            firstAudio!!)
-
+                            MainFragment.firstAudio!!)
                     }
                 }
+                try {
 
-                val intent = Intent()
-                intent.action = Intent.ACTION_SEND
-                intent.putExtra(Intent.EXTRA_STREAM, path)
+                    val targetShareIntents: MutableList<Intent> = ArrayList()
+//                    var intent = Intent()
 
-                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+//                    intent.action = Intent.ACTION_SEND
+//                    intent.putExtra(Intent.EXTRA_STREAM, path)
+////                    intent.putExtra(Intent.EXTRA_STREAM,Uri.parse("android.resource://funapps.fun.music.zarghtly/"+R.raw.zaghroutamomayaza))
+//
+//                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+//                    intent.type = "audio/mp3"
 
-                intent.type = "audio/mp3"
-                mcontext.startActivity(Intent.createChooser(intent, "ارسل الى"))
+                    val shareIntent = Intent()
+                    shareIntent.action = Intent.ACTION_SEND
+                    shareIntent.type = "text/plain"
+                    val resInfos: List<ResolveInfo> =
+                        mcontext.getPackageManager().queryIntentActivities(shareIntent, 0)
+
+                        if (!resInfos.isEmpty()) {
+                            for (resInfo in resInfos) {
+                                val packageName = resInfo.activityInfo.packageName
+                                Log.d("Package Name", packageName)
+                                if (packageName.contains("com.whatsapp") || packageName.contains("com.facebook.mlite") || packageName.contains(
+                                        "com.facebook.orca")
+                                ) {
+                                    val mintent = Intent()
+                                    mintent.setComponent(ComponentName(packageName,
+                                        resInfo.activityInfo.name))
+
+                                    mintent.action = Intent.ACTION_SEND
+                                    mintent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                                    mintent.type = "audio/mp3"
+                                    mintent.putExtra(Intent.EXTRA_STREAM, path)
+                                    mintent.setPackage(packageName)
+                                    targetShareIntents.add(mintent)
+                                }
+                            }
+                            if (!targetShareIntents.isEmpty()) {
+                                val chooserIntent =
+                                    Intent.createChooser(targetShareIntents.removeAt(0),
+                                        "ارسل الى ")
+                                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS,
+                                    targetShareIntents.toTypedArray())
+                                mcontext.startActivity(chooserIntent)
+                            } else {
+                                Toast.makeText(mcontext, "تعذر مشاركه التطبيق", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+
+
+                    } catch (e: Exception) {
+
+                        Log.d("sharebutton", "share: ${e.toString()}")
+                    }
+
+
+//
+                    //var intent = Intent()
+//                    intent.action = Intent.ACTION_SEND
+//                   intent.putExtra(Intent.EXTRA_STREAM, path)
+////                    intent.putExtra(Intent.EXTRA_STREAM,Uri.parse("android.resource://funapps.fun.music.zarghtly/"+R.raw.zaghroutamomayaza))
+//
+//                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+//                    intent.type = "audio/mp3"
+////
+//                    intent= Intent.createChooser(intent, "ارسل الى")
+
+
+//                    mcontext.startActivity(intent)
+
+
 
             }
 
@@ -178,10 +230,7 @@ class MusicAdapter(val musiclist: ArrayList<Music>, private val context: Context
                                 mediaPlayer!!.seekTo(progress)
                                 binding.seekbar.setProgress(progress);
                             }
-
                         }
-
-
                     }
 
                     override fun onStartTrackingTouch(seekBar: SeekBar?) = Unit
@@ -191,17 +240,11 @@ class MusicAdapter(val musiclist: ArrayList<Music>, private val context: Context
                             mediaPlayer!!.seekTo(binding.seekbar.getProgress());
 
                         }
-
-
                     }
                 }
             )
-
         }
-
-
     }
-
     fun clearMediaPlayer() {
         if (mediaPlayer != null) {
             mediaPlayer!!.stop()
@@ -209,58 +252,6 @@ class MusicAdapter(val musiclist: ArrayList<Music>, private val context: Context
             mediaPlayer!!.release()
             mediaPlayer = null
         }
-
-    }
-
-    fun saveaudiosToPhone() {
-
-        val downloadPath =
-            Environment.getExternalStoragePublicDirectory(DIRECTORY_DOWNLOADS).absolutePath + "/"
-        var inputStream = context.resources.openRawResource(R.raw.zarghoutaegypt)
-        firstAudio = File(downloadPath + "zarghoutaegypt.mp3")
-        copyInputStreamToFile(inputStream, firstAudio)
-
-        inputStream = context.resources.openRawResource(R.raw.zarhoutatunis)
-        secondAudio = File(downloadPath + "zarhoutatunis.mp3")
-        copyInputStreamToFile(inputStream, secondAudio)
-
-        inputStream = context.resources.openRawResource(R.raw.zarghoutamaroc)
-        thirdAudio = File(downloadPath + "zarghoutamaroc.mp3")
-        copyInputStreamToFile(inputStream, thirdAudio)
-
-        inputStream = context.resources.openRawResource(R.raw.zarghoutapalestine)
-        fourthAudio = File(downloadPath + "zarghoutapalestine.mp3")
-        copyInputStreamToFile(inputStream, fourthAudio)
-
-        inputStream = context.resources.openRawResource(R.raw.zargoutasoudan)
-        fifthAudio = File(downloadPath + "zargoutasoudan.mp3")
-        copyInputStreamToFile(inputStream, fifthAudio)
-
-        inputStream = context.resources.openRawResource(R.raw.zarghoutalebanon)
-        sixthAudio = File(downloadPath + "zarghoutalebanon.mp3")
-        copyInputStreamToFile(inputStream, sixthAudio)
-
-        inputStream = context.resources.openRawResource(R.raw.zarghoutaspecial)
-        seventhAudio = File(downloadPath + "zarghoutaspecial.mp3")
-        copyInputStreamToFile(inputStream, seventhAudio)
-
-        inputStream = context.resources.openRawResource(R.raw.zarghoutakhalegeya)
-        eighthAudio = File(downloadPath + "zarghoutakhalegeya.mp3")
-        copyInputStreamToFile(inputStream, eighthAudio)
-
-        inputStream = context.resources.openRawResource(R.raw.zahroutetelfarah)
-        ninethAudio = File(downloadPath + "zahroutetelfarah.mp3")
-        copyInputStreamToFile(inputStream, ninethAudio)
-
-        inputStream = context.resources.openRawResource(R.raw.zarghoutagroup)
-        tenthAudio = File(downloadPath + "zarghoutagroup.mp3")
-        copyInputStreamToFile(inputStream, tenthAudio)
-
-        inputStream = context.resources.openRawResource(R.raw.zarghoutaspecial)
-        eleventhAudio = File(downloadPath + "zarghoutaspecial.mp3")
-        copyInputStreamToFile(inputStream, eleventhAudio)
-
-
     }
 
 }
